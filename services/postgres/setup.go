2ae5373c8d5bbdfd18c618569fbb54e977edd6a6
microrutter <microrutter2514@hotmail.com>
2022-01-24 09:19:40 +0000

    Commented out future code that isn't used yet

--- services/postgres/setup.go
+++ services/postgres/setup.go
@@ -7,26 +7,26 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/nu7hatch/gouuid"
+	// "github.com/nu7hatch/gouuid"
 )
 
-type QuestionList struct {
-    Uuid uuid.UUID
-    Results []Question
-}
+// type QuestionList struct {
+//     Uuid uuid.UUID
+//     Results []Question
+// }
 
-type Question struct {
-    gorm.Model
-    Uuid string
-    Type string
-    Title string
-	Area string
-}
+// type Question struct {
+//     gorm.Model
+//     Uuid string
+//     Type string
+//     Title string
+// 	Area string
+// }
 
-func returnUUID() string {
-	uuid, _ := uuid.NewV4()
-	return uuid.String()
-}
+// func returnUUID() string {
+// 	uuid, _ := uuid.NewV4()
+// 	return uuid.String()
+// }
 
 func createConnection() (*gorm.DB, error)  {
     db, err := gorm.Open( "postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("PostGres_Host"), os.Getenv("PostGres_Port"), os.Getenv("PostGres_User"), os.Getenv("PostGres_Db"), os.Getenv("PostGres_Password")))
@@ -39,21 +39,21 @@ func createConnection() (*gorm.DB, error)  {
     return db, nil
 }
 
-func initialSetUp() {
-    _, exists := os.LookupEnv("RunSetUp")
+// func initialSetUp() {
+//     _, exists := os.LookupEnv("RunSetUp")
 
-    if exists {
+//     if exists {
 
-        db, err := createConnection()
+//         db, err := createConnection()
 
-        if err == nil {
-            defer db.Close()
+//         if err == nil {
+//             defer db.Close()
 
-            db.AutoMigrate(&Question{})
-        }
+//             db.AutoMigrate(&Question{})
+//         }
 
-    }
+//     }
     
-}
+// }
 
 
