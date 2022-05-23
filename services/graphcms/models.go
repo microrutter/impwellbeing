5b38474fe5e40a73deb97c98b219e2a6627c9207
microrutter <microrutter2514@hotmail.com>
2022-05-23 21:00:44 +0100

    Add blog model

--- services/graphcms/models.go
+++ services/graphcms/models.go
@@ -5,11 +5,11 @@ type CardResponse struct {
 }
 
 type Card struct {
-	Title *string `json:"title"`
-	Content1 *string `json:"content"`
-	Content2 *string `json:"content2"`
-	Content3 *string `json:"content3"`
-	Image *map[string]string `json:"image"`
+	Title    *string            `json:"title"`
+	Content1 *string            `json:"content"`
+	Content2 *string            `json:"content2"`
+	Content3 *string            `json:"content3"`
+	Image    *map[string]string `json:"image"`
 }
 
 type ResourceResponse struct {
@@ -17,14 +17,29 @@ type ResourceResponse struct {
 }
 
 type Resource struct {
-	Title *string `json:"title"`
-	Content1 *string `json:"content1"`
-	Content2 *string `json:"content2"`
-	Content3 *string `json:"content3"`
-	Image *map[string]string `json:"image"`
+	Title    *string            `json:"title"`
+	Content1 *string            `json:"content1"`
+	Content2 *string            `json:"content2"`
+	Content3 *string            `json:"content3"`
+	Image    *map[string]string `json:"image"`
 	Document *map[string]string `json:"document"`
-	HasLink *bool `json:"hasLink"`
-	Link1 *string `json:"link1"`
-	Link2 *string `json:"link2"`
-	Link3 *string `json:"link3"`
+	HasLink  *bool              `json:"hasLink"`
+	Link1    *string            `json:"link1"`
+	Link2    *string            `json:"link2"`
+	Link3    *string            `json:"link3"`
+}
+
+type Content struct {
+	Html *string `json:"html"`
+}
+
+type Blog struct {
+	Title       *string            `json:"title"`
+	Content     []Content          `json:"content"`
+	DateCreated *string            `json:"dateCreated"`
+	Image       *map[string]string `json:"titleImage"`
+}
+
+type BlogResponse struct {
+	Blogs []Blog `json:"blogs"`
 }
